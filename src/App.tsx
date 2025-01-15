a4e16e7ee10f2ff331752ed89fbb54d9efc73968
Jamesin Seidel <jamesin@chapterone.com>
2025-01-15 09:34:19 +0000

    Add routes configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { useRoutes, Routes, Route, RouteObject } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
+import config from "../tempo.config.json";
+const routes = config.routes as unknown as RouteObject[];
 
 function App() {
   return (
