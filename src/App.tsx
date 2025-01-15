eeb78eee246e11218c806150096687df8fb5f18a
Jamesin Seidel <jamesin@chapterone.com>
2025-01-15 17:03:29 +0000

    Add auth callback handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route, RouteObject } from "react-router-dom";
 import Home from "./components/home";
 import config from "../tempo.config.json";
+import { AuthCallback } from './components/AuthCallback'
 const routes = config.routes as unknown as RouteObject[];
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
       {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
     </div>
